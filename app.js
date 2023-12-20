f0a457f930319825b22bdafd32ccbce007e884e3
Rapong <gnopgnopar@gmail.com>
2023-12-20 21:29:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fileRoutes = require('./routes/fileRoutes');
 const userRoutes = require('./routes/userRoutes');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
 const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
